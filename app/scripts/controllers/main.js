d5b05120d994e5ca754f040c47826cc41c4cda19
Prashant Khanduri <pkhanduri@jawbone.com>
2015-01-02 10:35:46 -0800

    ng-src directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,16 +15,25 @@ angular.module('gemstoreApp')
         name: 'Dodecahedron',
         price: 2.95,
         soldOut: false,
+        images: [
+          'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif'
+        ],
         description: 'Some description'
       }, {
         name: 'Tetrahedron',
         price: 1.00,
         soldOut: true,
+        images: [
+          'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif'
+        ],
         description: 'Some tetra description'
       }, {
         name: 'Quadrahedron',
         price: 5.00,
         soldOut: false,
+        images: [
+          'http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-03.gif'
+        ],
         description: 'Some tetra description'
       }
     ]
